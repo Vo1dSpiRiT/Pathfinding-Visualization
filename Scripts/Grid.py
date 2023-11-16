64cdd332d7210e45bc6a490a83e4434e70dcd3e6
Vo1dSpiRiT <yassine.brahem081@gmail.com>
2023-11-17 00:32:54 +0100

    Update Grid.py

--- Scripts/Grid.py
+++ Scripts/Grid.py
@@ -2,20 +2,21 @@ from Scripts.Node import Node
 import numpy as np
 
 class Grid():
-    def __init__(self, rows, cols, startNodePosition, endNodePosition):
+    def __init__(self, rows, cols, nodeDimensions, startNodePosition, endNodePosition):
         self.rows = rows
         self.cols = cols
         self.startNodePosition = startNodePosition
         self.endNodePosition = endNodePosition
-        self.nodes = self.getNodes()
+        self.nodes = self.getNodes(nodeDimensions)
         
-    def getNodes(self):
-        nodes = np.array((self.rows, self.cols), dtype=object)
+    def getNodes(self, nodeDimensions):
+        nodes = np.empty((self.rows, self.cols), dtype=object)
         for i in range(self.rows):
             for j in range(self.cols):
-                nodes[i, j] = Node(i, j)
-        nodes[self.startNodePosition.x, self.startNodePosition.y].isStart = True
-        nodes[self.endNodePosition.x, self.endNodePosition.y].isEnd = True
+                nodes[i, j] = Node(i, j, nodeDimensions)
+        nodes[self.startNodePosition[0], self.startNodePosition[1]].isStart = True
+        nodes[self.endNodePosition[0], self.endNodePosition[1]].isEnd = True
+        return nodes
     
     def __getitem__(self, index):
         return self.nodes[index]
@@ -27,9 +28,13 @@ class Grid():
         self.nodes = self.getNodes()
         
     def update(self, startNodePosition, endNodePosition):
-        self.nodes[self.startNodePosition.x, self.startNodePosition.y].isStart = False
-        self.nodes[self.endNodePosition.x, self.endNodePosition.y].isEnd = False
+        self.nodes[self.startNodePosition[0], self.startNodePosition[1]].isStart = False
+        self.nodes[self.endNodePosition[0], self.endNodePosition[1]].isEnd = False
         self.startNodePosition = startNodePosition
         self.endNodePosition = endNodePosition
-        self.nodes[self.startNodePosition.x, self.startNodePosition.y].isStart = True
-        self.nodes[self.endNodePosition.x, self.endNodePosition.y].isEnd = True
+        self.nodes[self.startNodePosition[0], self.startNodePosition[1]].isStart = True
+        self.nodes[self.endNodePosition[0], self.endNodePosition[1]].isEnd = True
+        
+    def draw(self, surface):
+        for node in self.nodes.flatten():
+            node.draw(surface)
