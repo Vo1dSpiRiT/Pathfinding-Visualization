f9c0985b69f99f78ee310357021e6b01c693a3c2
Vo1dSpiRiT <yassine.brahem081@gmail.com>
2023-11-21 23:23:20 +0100

    Implement speed controls
    fix node type placement conditions

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import numpy as np
 from Scripts.Grid import Grid
 from Scripts.Dijkstra import Dijkstra
 
-resolution = Vector2(1000, 700)
+resolution = Vector2(1500, 900)
 WIN = pygame.display.set_mode((int(resolution.x), int(resolution.y)))
 pygame.display.set_caption("Pathfinding Visualizer")
 
 FPS_Font = pygame.font.SysFont('sans-serif', 40)
-TARGET_FPS = 60
 
 def draw_window(TimePerFrame):
     WIN.fill((20, 80, 145))
@@ -28,12 +27,12 @@ def drawFPS(TimePerFrame):
     FPS_Text = FPS_Font.render("FPS: "  + str(CurrentFPS), 1, (255, 255, 255))
     WIN.blit(FPS_Text, (int(resolution.x) - 150, 10))
     
-gridDimensions = [40, 30] #rows, columns
+gridDimensions = [150, 90] #rows, columns
 nodeDimensions = [resolution[0]/gridDimensions[0], resolution[1]/gridDimensions[1]]
 grid = Grid(gridDimensions[0], gridDimensions[1], nodeDimensions, Vector2(0,0), Vector2(4,4))
     
 def drawlines(surface):
-    lineColor = (20, 20, 145)
+    lineColor = (20, 70, 145)
     for i in range(gridDimensions[1]+1):
         pygame.draw.line(surface, lineColor,
                         (0, int(i * nodeDimensions[1])),
@@ -51,53 +50,20 @@ def main():
     blocks = {"S": "start", "E": "end", "W": "wall", "G": "weight"}
     weight = 5
     mouseHold = False
-    selectedBlock = "end"
+    selectedBlock = "wall"
+    timePassed = 0
+    speed = 1
     currentAnimatedNode = None
     while running:
-        TimePerFrame  = clock.tick(60) * .001
-        dt = TimePerFrame * TARGET_FPS
-
+        TimePerFrame  = clock.tick(300) * .001
+        dt = TimePerFrame
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 
-            mousePosition = pygame.mouse.get_pos()
-            mousePressed = pygame.mouse.get_pressed()
-            if mousePressed[0]:
-                if algorithmComplete:
-                    algorithmComplete = False
-                    grid.reset()
-                    
-                selectedNode = grid.getPressedNode(mousePosition)
-                if not (selectedNode.isEnd or selectedNode.isStart):
-                    if selectedBlock == "start":
-                        grid.changeStartNode(selectedNode)
-                    if selectedBlock == "end":
-                        grid.changeEndNode(selectedNode)
-                        
-                    if selectedBlock == "wall":
-                        if not mouseHold:
-                            selectedState = not selectedNode.isWall
-                        if selectedNode.weight and selectedState:
-                            selectedNode.weight = 0
-                        selectedNode.isWall = selectedState
-                        
-                    if selectedBlock == "weight":
-                        if not mouseHold:
-                            if selectedNode.weight != weight:
-                                selectedState = True
-                            else:
-                                selectedState = False
-                        if selectedState and not selectedNode.isWall:
-                            selectedNode.weight = weight
-                        else:
-                            selectedNode.weight = 0
-                mouseHold = True
-            else:
-                mouseHold = False
-                    
+            keysPressed = pygame.key.get_pressed()
             if event.type == pygame.KEYDOWN:
-                keysPressed = pygame.key.get_pressed()
                 if keysPressed[pygame.K_s]:
                     selectedBlock = blocks["S"]
                 if keysPressed[pygame.K_e]:
@@ -113,23 +79,74 @@ def main():
                     if not algorithmComplete:
                         visitedNodesInOrder, shortestPath = Dijkstra(grid)
                         algorithmComplete = True
+                        timePassed = 0
+                        dt = 0
                     else:
                         algorithmComplete = False
                         grid.reset()
+        
+        if keysPressed[pygame.K_UP]:
+            speed += 1
+        if keysPressed[pygame.K_DOWN]:
+            if speed > 1:
+                speed -= 1
                 
+        mousePosition = pygame.mouse.get_pos()
+        mousePressed = pygame.mouse.get_pressed()
+        if mousePressed[0]:
+            if algorithmComplete:
+                algorithmComplete = False
+                grid.reset()
+                    
+            selectedNode = grid.getPressedNode(mousePosition)
+            if not (selectedNode.isEnd or selectedNode.isStart):
+                if not selectedNode.isWall:
+                    if selectedBlock == "start":
+                        grid.changeStartNode(selectedNode)
+                    if selectedBlock == "end":
+                        grid.changeEndNode(selectedNode)
+                    
+                if selectedBlock == "wall":
+                    if not mouseHold:
+                        selectedState = not selectedNode.isWall
+                    if selectedNode.weight and selectedState:
+                        selectedNode.weight = 0
+                    selectedNode.isWall = selectedState
+                    
+                if selectedBlock == "weight":
+                    if not mouseHold:
+                        if selectedNode.weight != weight:
+                            selectedState = True
+                        else:
+                            selectedState = False
+                    if selectedState and not selectedNode.isWall:
+                        selectedNode.weight = weight
+                    else:
+                        selectedNode.weight = 0
+            mouseHold = True
+        else:
+            mouseHold = False
+                
+        
         if algorithmComplete:
-            if len(visitedNodesInOrder) or len(shortestPath):
-                if len(visitedNodesInOrder):
-                    if currentAnimatedNode:
-                        currentAnimatedNode.isSelected = False
-                    currentAnimatedNode = visitedNodesInOrder[0]
-                    visitedNodesInOrder = np.delete(visitedNodesInOrder, 0)
-                    currentAnimatedNode.animated = True
-                elif len(shortestPath):
-                    currentAnimatedNode = shortestPath[-1]
-                    currentAnimatedNode.isPath = True
-                    shortestPath = np.delete(shortestPath, -1)
-                currentAnimatedNode.isSelected = True
+            timePassed += dt
+            ticks = int(timePassed // (1/speed))
+            if ticks:
+                timePassed %= (1/speed)
+                for tick in range(ticks):
+                    if len(visitedNodesInOrder) or len(shortestPath):
+                        if len(visitedNodesInOrder):
+                            if currentAnimatedNode:
+                                currentAnimatedNode.isSelected = False
+                            currentAnimatedNode = visitedNodesInOrder[0]
+                            visitedNodesInOrder = np.delete(visitedNodesInOrder, 0)
+                            currentAnimatedNode.animated = True
+                        elif len(shortestPath):
+                            currentAnimatedNode = shortestPath[-1]
+                            currentAnimatedNode.isPath = True
+                            shortestPath = np.delete(shortestPath, -1)
+                        currentAnimatedNode.isSelected = True
+                
                     
         draw_window(TimePerFrame)
 
